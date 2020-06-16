4a25b8ea5880e14847ea3c694901c08385d17dd7
Julie Desaintléger <jdesaintleger@excilys.com>
2020-06-16 15:33:21 +0200

    Add model

---
+++ gatling-test/src/main/java/com/excilys/formation/cdb/model/Company.java
@@ -0,0 +1,31 @@
+package com.excilys.formation.cdb.model;
+
+/**
+ * Une compagnie est le fabricant d'un ordinateur.
+ * 
+ * @author julie
+ *
+ */
+public class Company {
+    private long id;
+    private String name;
+
+    public Company(String name) {
+	super();
+	this.name = name;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    @Override
+    public String toString() {
+	return "Company [id=" + id + ", name=" + name + "]";
+    }
+
+}
---
+++ gatling-test/src/main/java/com/excilys/formation/cdb/model/Computer.java
@@ -0,0 +1,79 @@
+package com.excilys.formation.cdb.model;
+
+import java.util.Date;
+
+/**
+ * Un ordinateur doit obligatoirement avoir au moins un nom. Si cela est
+ * possible, l'ordinateur peut avoir la date à laquelle il a été introduit, la
+ * date a laquelle il a été arrêté et son constructeur.
+ * 
+ * @author julie
+ *
+ */
+public class Computer {
+    private long id;
+    private String name;
+    private Date introduced;
+    private Date discontinued;
+    private long idCompany;
+
+    public Computer(String name) {
+	super();
+	this.name = name;
+    }
+
+    public Computer(String name, Date introduced, Date discontinued, long idCompany) {
+	super();
+	this.name = name;
+	this.introduced = introduced;
+	this.discontinued = discontinued;
+	this.idCompany = idCompany;
+    }
+
+    public long getId() {
+	return id;
+    }
+
+    public void setId(long id) {
+	this.id = id;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public Date getIntroduced() {
+	return introduced;
+    }
+
+    public void setIntroduced(Date introduced) {
+	this.introduced = introduced;
+    }
+
+    public Date getDiscontinued() {
+	return discontinued;
+    }
+
+    public void setDiscontinued(Date discontinued) {
+	this.discontinued = discontinued;
+    }
+
+    public long getIdCompany() {
+	return idCompany;
+    }
+
+    public void setIdCompany(long idCompany) {
+	this.idCompany = idCompany;
+    }
+
+    @Override
+    public String toString() {
+	return "Computer [id=" + id + ", name=" + name + ", introduced=" + introduced + ", discontinued=" + discontinued
+		+ ", idCompany=" + idCompany + "]";
+    }
+
+}
