f77e2ae20f3e438e158464f2b65120500af5c413
Julie Desaintleger <jdesaintleger@excilys.com>
2020-06-22 16:24:33 +0200

    Replace structure of project

---
+++ src/main/java/com/excilys/formation/cdb/model/Company.java
@@ -0,0 +1,36 @@
+package com.excilys.formation.cdb.model;
+
+/**
+ * Une compagnie est le fabricant d'un ordinateur.
+ * 
+ * @author julie
+ *
+ */
+public class Company {
+    private long id;
+    private String name;
+
+    public Company(long id, String name) {
+	this.id = id;
+	this.name = name;
+    }
+
+    public Company(String name) {
+	super();
+	this.name = name;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    @Override
+    public String toString() {
+	return "Company [id=" + id + ", name=" + name + "]";
+    }
+
+}
