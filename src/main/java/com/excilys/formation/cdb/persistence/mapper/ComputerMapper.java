ee85711245bd98f1fbec8f33a4d4155aa4344560
Julie Desaintleger <jdesaintleger@excilys.com>
2020-07-08 12:26:04 +0200

    Change implementation for ComputerMapper after changing model company_id to company

--- src/main/java/com/excilys/formation/cdb/persistence/mapper/ComputerMapper.java
+++ src/main/java/com/excilys/formation/cdb/persistence/mapper/ComputerMapper.java
@@ -2,20 +2,23 @@ package com.excilys.formation.cdb.persistence.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.excilys.formation.cdb.model.Company;
 import com.excilys.formation.cdb.model.Computer;
 
 public class ComputerMapper {
-    private static Computer newComputer;
     private static final String ID_COMPUTER = "id";
     private static final String NAME = "name";
     private static final String INTRODUCED = "introduced";
     private static final String DISCONTINUED = "discontinued";
     private static final String COMPANY_ID = "company_id";
-    private static Logger logger = LoggerFactory.getLogger(ComputerMapper.class);
+    private static final String COMPANY_NAME = "company_name";
+
+    private static Logger logger = LoggerFactory.getLogger(CompanyMapper.class);
 
     /**
      * S'occupe de la conversion du résultat en entité
@@ -24,27 +27,40 @@ public class ComputerMapper {
      * @return une entité Computer correspondante
      */
     public static Computer map(ResultSet resultSet) {
+	Computer computer = new Computer();
 	try {
-	    newComputer = new Computer(resultSet.getLong(ID_COMPUTER), resultSet.getString(NAME));
-	    checkIsNullIntroduced(resultSet);
-	    checkDiscontinued(resultSet);
-	    newComputer.setIdCompany(resultSet.getLong(COMPANY_ID));
-	} catch (SQLException e) {
-	    logger.error("Erreur -> Mapping Computer");
+	    Long id = resultSet.getLong(ID_COMPUTER);
+	    String name = resultSet.getString(NAME);
+	    LocalDate introduced = null;
+	    introduced = checkIsNullIntroduced(resultSet, introduced);
+	    LocalDate discontinued = null;
+	    discontinued = checkDiscontinued(resultSet, discontinued);
+	    Long company_id = resultSet.getLong(COMPANY_ID);
+	    String company_name = resultSet.getString(COMPANY_NAME);
+	    computer.setId(id);
+	    computer.setName(name);
+	    computer.setIntroduced(introduced);
+	    computer.setDiscontinued(discontinued);
+	    computer.setCompany(new Company.Builder().setId(company_id).setName(company_name).build());
+	} catch (Exception e) {
+	    logger.error("Erreur -> Mapping ResultSet to Computer", e.getMessage());
 	}
-	return newComputer;
+	return computer;
     }
 
-    private static void checkDiscontinued(ResultSet resultSet) throws SQLException {
+    private static LocalDate checkDiscontinued(ResultSet resultSet, LocalDate discontinued) throws SQLException {
 	if (resultSet.getDate(DISCONTINUED) != null
 		&& resultSet.getDate(DISCONTINUED).after(resultSet.getDate(INTRODUCED))) {
-	    newComputer.setDiscontinued(resultSet.getDate(DISCONTINUED).toLocalDate());
+	    discontinued = resultSet.getDate(DISCONTINUED).toLocalDate();
 	}
+	return discontinued;
     }
 
-    private static void checkIsNullIntroduced(ResultSet resultSet) throws SQLException {
+    private static LocalDate checkIsNullIntroduced(ResultSet resultSet, LocalDate introduced) throws SQLException {
 	if (resultSet.getDate(INTRODUCED) != null) {
-	    newComputer.setIntroduced(resultSet.getDate(INTRODUCED).toLocalDate());
+	    introduced = resultSet.getDate(INTRODUCED).toLocalDate();
 	}
+	return introduced;
     }
+
 }
