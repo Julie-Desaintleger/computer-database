dc14084d5961b428ab87d43b9ea218b9bb9d98c9
Julie Desaintleger <jdesaintleger@excilys.com>
2020-09-11 10:55:11 +0200

    Add config for QueryDSL

--- src/main/java/com/excilys/formation/cdb/config/DataConfig.java
+++ src/main/java/com/excilys/formation/cdb/config/DataConfig.java
@@ -11,8 +11,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan(basePackages = { "com.excilys.formation.cdb.persistence" })
 @PropertySource("classpath:datasource.properties")
 public class DataConfig {
@@ -35,5 +42,24 @@ public class DataConfig {
 	driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
 	driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
 	return driverManagerDataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+	JpaTransactionManager transactionManager = new JpaTransactionManager();
+	transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+	return transactionManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+	em.setDataSource(dataSource());
+	em.setPackagesToScan(new String[] { "com.excilys.computerDatabase.models" });
+
+	JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	em.setJpaVendorAdapter(vendorAdapter);
+	return em;
     }
 }
