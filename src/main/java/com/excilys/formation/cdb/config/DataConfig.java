378c662f989703a044d204ca11cc0e42f0940daa
Julie Desaintleger <jdesaintleger@excilys.com>
2020-09-14 11:28:44 +0200

    Modify DataConfig for datasource

--- src/main/java/com/excilys/formation/cdb/config/DataConfig.java
+++ src/main/java/com/excilys/formation/cdb/config/DataConfig.java
@@ -1,16 +1,10 @@
 package com.excilys.formation.cdb.config;
 
-import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -18,30 +12,19 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = { "com.excilys.formation.cdb.persistence" })
-@PropertySource("classpath:datasource.properties")
 public class DataConfig {
     private final Logger logger = LoggerFactory.getLogger(DataConfig.class);
 
-    @Autowired
-    Environment environment;
-
-    private final String URL = "jdbcUrl";
-    private final String USER = "dataSource.user";
-    private final String DRIVER = "driverClassName";
-    private final String PASSWORD = "dataSource.password";
-
     @Bean
-    public DataSource dataSource() {
-	logger.info("new Datasource");
-	DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-	driverManagerDataSource.setUrl(environment.getProperty(URL));
-	driverManagerDataSource.setUsername(environment.getProperty(USER));
-	driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
-	driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
-	return driverManagerDataSource;
+    public HikariDataSource dataSource() {
+	logger.info("new datasource");
+	return new HikariDataSource(new HikariConfig("/datasource.properties"));
     }
 
     @Bean
@@ -56,7 +39,7 @@ public class DataConfig {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 	LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 	em.setDataSource(dataSource());
-	em.setPackagesToScan(new String[] { "com.excilys.computerDatabase.models" });
+	em.setPackagesToScan(new String[] { "com.excilys.formation.cdb.model" });
 
 	JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 	em.setJpaVendorAdapter(vendorAdapter);
